c0515f8082e71d542a32134663b245d66e60a361
Cédric <cedric.demongivert@gmail.com>
2020-05-11 10:53:51 -0400

    removing distribution folder

--- distribution/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { EntityComponentSystem } from './EntityComponentSystem';
-export { System } from './System';
-export { EntityComponentSystemBuilder } from './EntityComponentSystemBuilder';
-export { Entity } from './Entity';
-export { Tag } from './Tag';
-export { ComponentType } from './ComponentType';
-export { Component } from './Component';
-export { MetaEntity } from './MetaEntity';
